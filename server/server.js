8dc0471021fdb33a8ea06e24d4242d6182c66e0e
Ashish Suman <ashish.suman927@gamil.com>
2022-06-29 18:16:21 +0530

    set up mongo databse

--- server/server.js
+++ server/server.js
@@ -6,6 +6,18 @@ const mongoose = require("mongoose");
 //---------------end-of-requiring modules------------------
 
 
+//---------------setting up database----------------
+const dbUrl = "mongodb://localhost:27017/online-judge";
+
+mongoose.connect(dbUrl);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connetcted");
+});
+//---------------end-of-setting-up-database----------------
+
 
 //--------------starting-up-server-------------------------
 const port = 5000;
