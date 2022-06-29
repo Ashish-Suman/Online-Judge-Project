ad65aa9a4873cb76f089c087d429c9452de642e1
Ashish Suman <ashish.suman927@gamil.com>
2022-06-29 18:15:21 +0530

    set up react app

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
+//---------------requiring modules------------------
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+
+//---------------end-of-requiring modules------------------
 
 
+
+//--------------starting-up-server-------------------------
 const port = 5000;
 app.listen(port, () => {
     console.log(`Serving on por ${port}`);
 })
+
+//--------------end-of-starting-up-server-------------------------
